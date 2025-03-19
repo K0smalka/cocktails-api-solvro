649f0035b47a648d962bdae2528cd2963437524c
K0smalka <284325@student.pwr.edu.pl>
2025-03-19 13:03:22 +0100

    changes in database.js

--- config/database.js
+++ config/database.js
@@ -1,6 +1,10 @@
 import {Sequelize} from "sequelize";
 
-export const sequelize = new Sequelize("postgres", "postgres", "password", {
+const DB_NAME = process.env.DB_NAME || "postgres";
+const DB_USER = process.env.DB_USER || "postgres";
+const DB_PASSWORD = process.env.DB_PASSWORD || "password";
+
+export const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: "localhost",
     dialect: "postgres",
     logging: false,
